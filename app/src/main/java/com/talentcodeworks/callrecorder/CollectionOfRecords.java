996ea572a31f10ef6a5444ddcd1715b7ebff4200
Jenya95 <sanevichj@gmail.com>
2016-02-15 18:39:49 +0300

    update collection class

--- app/src/main/java/com/talentcodeworks/callrecorder/CollectionOfRecords.java
+++ app/src/main/java/com/talentcodeworks/callrecorder/CollectionOfRecords.java
@@ -1,12 +1,12 @@
 package com.talentcodeworks.callrecorder;
 
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.gms.common.api.GoogleApiClient;
 
 
 /**
@@ -17,18 +17,33 @@ import android.gms.common.api.GoogleApiClient;
  * getPhoneType() - тип сети радиосвязи
  * getLatitude() - широта
  * getLongitude() - долгота
- *
+ * setName(String, String) - запись имени файла
  */
-public class CollectionOfRecords {
+public class CollectionOfRecords implements LocationListener {
 
     private Context context;
     private TelephonyManager manager=(TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-    private GoogleApiClient mGoogleApiClient;
+    protected LocationManager locationManager;
+    public double latitude=0;
+    public double longitude=0;
+    public String nameOfFile = "";
+    public String netType = "";
+    public String IMEI = "";
+    public String phoneType = "";
 
 
+    public CollectionOfRecords(Context _context, String prefix, String suffix) {
+        context = _context; //здесь при вызове конструкотра в коллекцию должна добавляться запись
+                            //с необходимыми полями
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
-    public CollectionOfRecords(Context _context) {
-        context = _context;
+        latitude = getLatitude();
+        longitude = getLongitude();
+        nameOfFile = setName(prefix,suffix);
+        netType = getNetType();
+        IMEI = getIMEINumber();
+        phoneType = getPhoneType();
     }
 
     public String getIMEINumber()
@@ -55,30 +70,51 @@ public class CollectionOfRecords {
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+
     public String getPhoneType()
     {
         int i = manager.getPhoneType();
         switch (i) {
         case 0: return "No phone radio";
-            break;
         case 1: return "Phone radio is GSM";
-            break;
         case 2: return "Phone radio is CDMA";
-            break;
         case 3: return "Phone is via SIP";
-            break;
+        default: return "Unknown";
         }
     }
 
     public double getLatitude()
     {
-        return 0;
+        return latitude;
     }
 
     public double getLongitude()
     {
-        return 0;
+        return longitude;
     }
 
-
+    public String setName(String a, String b)
+    {
+        return a+b;
+    }
 }
