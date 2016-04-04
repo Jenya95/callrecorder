ad9aac7f898af908c1345ca2bbb42944bcbf1bf6
Jenya95 <sanevichj@gmail.com>
2016-04-04 21:30:19 +0300

    signal strength

---
+++ app/src/main/java/com/talentcodeworks/callrecorder/AndroidPhoneStateListener.java
@@ -0,0 +1,21 @@
+package com.talentcodeworks.callrecorder;
+
+import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
+
+public class AndroidPhoneStateListener extends PhoneStateListener {
+    public int signalStrengthValue;
+
+    @Override
+    public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+        super.onSignalStrengthsChanged(signalStrength);
+        if (signalStrength.isGsm()) {
+            if (signalStrength.getGsmSignalStrength() != 99)
+                signalStrengthValue = signalStrength.getGsmSignalStrength() * 2 - 113;
+            else
+                signalStrengthValue = signalStrength.getGsmSignalStrength();
+        } else {
+            signalStrengthValue = signalStrength.getCdmaDbm();
+        }
+    }
+}
